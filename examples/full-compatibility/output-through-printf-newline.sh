f91165b8b9552e7d2198cf6b194be958e73d92e0
Piero Sebastiani <77357494+psebastiani2021@users.noreply.github.com>
2023-06-23 18:57:18 -0500

    Create output-through-printf-newline.sh

---
+++ examples/full-compatibility/output-through-printf-newline.sh
@@ -0,0 +1,8 @@
+# Output through printf with newline in Shell
+# The printf keyword lets you print something in the console, but it has the ability to include new lines through \n
+# Example:
+printf "Multiple line command\nThis is cool!"
+# The output will be like this:
+# Multiple line command
+# This is cool!
+# The other methods doesn't support new lines.
