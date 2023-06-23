f1e4378c16fc642a1ad3559383c7f5c5e6ebe504
psebastiani <77357494+psebastiani2021@users.noreply.github.com>
2023-06-23 18:22:17 -0500

    Create basic-input.sh

---
+++ examples/full-compatibility/basic-input.sh
@@ -0,0 +1,5 @@
+# Basic Input Shell script. 
+# This doesn't have any functions or utilities as it is just a basic input. 
+$name = Read-Host 'Input question'
+# This will output 'Input question' and a text field.
+# Try this on your PowerShell!
