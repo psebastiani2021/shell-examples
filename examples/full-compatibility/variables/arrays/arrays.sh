0292592d00a86df166577368072f572eb1668e1b
Piero Sebastiani <77357494+psebastiani2021@users.noreply.github.com>
2023-06-25 17:50:13 -0500

    Create arrays.sh

---
+++ examples/full-compatibility/variables/arrays/arrays.sh
@@ -0,0 +1,7 @@
+# To create a basic array with many values, write a normal variable and assign the '@()' value to it. Between the parentheses you will enter the array's variables.
+$Array = @(
+ "First array value"
+ "Second array value"
+ "Third array value"
+ )
+# If a warning pop-up shows when you copy the content, click on "Paste anyway".
