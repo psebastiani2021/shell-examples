91717f631cbc0b4522f6dd6d529efdfb320f54d7
Piero Sebastiani <77357494+psebastiani2021@users.noreply.github.com>
2023-06-25 18:13:11 -0500

    Update and rename through-quotes.sh to quotes.sh

--- examples/full-compatibility/output/quotes.sh
+++ examples/full-compatibility/output/quotes.sh
@@ -1,4 +1,4 @@
-# Output through quotes, just like in JS
+# You can output something through quotes
 "myString"
 # The output will be myString
 # There is another way of doing this. Check output-through-echo.sh
