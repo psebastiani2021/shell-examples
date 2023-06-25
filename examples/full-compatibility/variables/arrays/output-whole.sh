fa2307c7ed9cdb440a30678edc621c0f7b6ca9d1
Piero Sebastiani <77357494+psebastiani2021@users.noreply.github.com>
2023-06-25 17:55:53 -0500

    Create output-whole.sh

---
+++ examples/full-compatibility/variables/arrays/output-whole.sh
@@ -0,0 +1,9 @@
+# To output EVERY item in an array, type the array as if it would be a variable:
+$favNumbers = @(
+ "13"
+ "834"
+ "9"
+ "91"
+)
+$favNumbers
+# The output will be 13, 834, 9, and 91, separated through line breaks.
