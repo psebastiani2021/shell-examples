b51bb309bac2d2600f519a61038a4036c6d9eeb0
Piero Sebastiani <77357494+psebastiani2021@users.noreply.github.com>
2023-06-25 16:52:11 -0500

    Create multiple-print.sh

---
+++ examples/full-compatibility/variables/multiple-print.sh
@@ -0,0 +1,7 @@
+# If a variable has more than one value, you can print them all at once.
+# Try running $myVar, which has the values '1, 2, 3' assigned to it, and look at the output:
+$myVar = 1, 2, 3
+# The output will be:
+# 1
+# 2
+# 3
