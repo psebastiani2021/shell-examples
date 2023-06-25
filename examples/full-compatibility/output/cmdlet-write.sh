18b47911ef1c96b5634d77b5500b704d79fb5234
Piero Sebastiani <77357494+psebastiani2021@users.noreply.github.com>
2023-06-25 18:02:29 -0500

    versions: Echo primarily used in Bash

---
+++ examples/full-compatibility/output/cmdlet-write.sh
@@ -0,0 +1,5 @@
+# You can output something using Write-Output cmdlet in Shell
+Write-Output Hello World!
+# or
+Write-Host Hello World!
+# Tip: Using a comma in this cmdlet generates a new line break.
--- examples/full-compatibility/output/through-echo.sh
+++
@@ -1,4 +0,0 @@
-# Output using the echo method, just like in PHP
-echo myString
-# The output will be "myString"
-# There is another way of doing this. Check output-through-quotes.sh
