39f1cffa70ec389da24cbbd6c97c1e99e5fb4779
Piero Sebastiani <77357494+psebastiani2021@users.noreply.github.com>
2023-06-25 17:54:22 -0500

    Create output-single-item.sh

---
+++ examples/full-compatibility/variables/arrays/output-single-item.sh
@@ -0,0 +1,11 @@
+# To output a single item of an array, type between two square brackets the number of the array's item. For example:
+$Array = @(
+ "John"
+ "Zarah"
+ "Paul"
+ "Brittany"
+)
+# To output $Array's first value you need to type 0 instead of 1.
+$Array[0]
+# The output will be John.
+ 
