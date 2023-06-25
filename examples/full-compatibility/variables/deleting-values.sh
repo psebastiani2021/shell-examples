2ba3ba1c225593d1ab599c33a6926d551967aeaf
Piero Sebastiani <77357494+psebastiani2021@users.noreply.github.com>
2023-06-25 17:03:32 -0500

    Create deleting-values.sh

---
+++ examples/full-compatibility/variables/deleting-values.sh
@@ -0,0 +1,4 @@
+# To delete the value of a variable use the Clear-Variable cmdlet.
+# This is different than clearing a variable; the variable's value will become $null.
+Clear-Variable -Name myVar
+# If you try to output $myVar's result, nothing will happen.
