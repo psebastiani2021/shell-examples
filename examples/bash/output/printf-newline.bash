99633b9a4c57525ba84d2903b33834021235fe9d
Piero Sebastiani <77357494+psebastiani2021@users.noreply.github.com>
2023-06-25 18:00:20 -0500

    Update printf-newline.bash

--- examples/bash/output/printf-newline.bash
+++ examples/bash/output/printf-newline.bash
@@ -1,7 +1,5 @@
-# Output through printf with newline in Bash
 # The printf keyword lets you print something in the console, but it has the ability to include new lines through \n
 printf "Multiple line command\nThis is cool!"
 # The output will be like this:
 # Multiple line command
 # This is cool!
-# You can't do this with a common Windows PowerShell. You need Bash.
